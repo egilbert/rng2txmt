6c4ba81d16036d6779b2916ac7265746c1434c76
Édouard Gilbert <edouard.gilbert@gmail.com>
2009-04-10 15:43:18 +0200

    Refactor repertory disposition.
    --HG--
    rename : rng2txmt.xsl => src/rng2txmt.xsl
    rename : xml2plist.xsl => src/xml2plist.xsl

--- rng2txmt.sh
+++ rng2txmt.sh
@@ -13,16 +13,19 @@ then
 fi
 
 base_dir="."
+source_dir="$base_dir/src"
 result_dir="$base_dir/Generated Language Grammars"
 name=`basename -s .rng $1`;
 xml_result="$result_dir/$name.plist.xml"
 plist_result="$result_dir/$name.plist"
+rng2txmt_transform="$source_dir/rng2txmt.xsl"
+xml2plist_transform="$source_dir/xml2plist.xsl"
 
 echo "Generating grammar for $name..."
-xsltproc --output "$xml_result" rng2txmt.xsl "$1";
+xsltproc --output "$xml_result" "$xsl_transform" "$1";
 echo "Checking validity of XML Property List..."
 plutil -lint "$xml_result";
 echo "Generating old style Property List..."
 # FIXME auto indent $plist_result.
-xsltproc --output "$plist_result" xml2plist.xsl "$xml_result";
+xsltproc --output "$plist_result" "$xml2plist_transform" "$xml_result";
 echo "Done."
